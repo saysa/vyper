d98936ea56f9a99db58315c14a967de8fd061381
Saysa <saysabou@gmail.com>
2013-09-13 10:55:10 +0200

    Model theme

--- application/models/Theme.php
+++ application/models/Theme.php
@@ -6,7 +6,22 @@ use Framework\Shared\Model;
 
 class Theme extends Model {
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @primary
+	 * @type autonumber
+	 */
 	protected $_id;
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type text
+	 * @length 255
+	 *
+	 * @validate required, min(3)
+	 * @label title
+	 */
 	protected $_title;
 }
