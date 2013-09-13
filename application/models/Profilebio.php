2b35a8124304942ab33d2bf4ad4ba22c27977af4
Saysa <saysabou@gmail.com>
2013-09-13 17:03:57 +0200

    Model Profilebio

--- application/models/Profilebio.php
+++ application/models/Profilebio.php
@@ -6,13 +6,48 @@ use Framework\Shared\Model;
 
 class Profilebio extends Model {
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @primary
+	 * @type autonumber
+	 */
 	protected $_id;
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type integer
+	 */
+	protected $_artist;
+	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type text
+	 */
 	protected $_profile;
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type text
+	 */
 	protected $_biography;
 	
-	protected $_authorID; // id's
+	/**
+	 * @column
+	 * @readwrite
+	 * @type text
+	 * @length 255
+	 */
+	protected $_author; 
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type text
+	 * @length 255
+	 */
 	protected $_translator;
 }
