1c1d1682c5492be916c0588a7bc5b73897cd7876
Saysa <saysabou@gmail.com>
2014-01-13 11:45:02 +0100

    model tour complete annotations

--- application/models/Tour.php
+++ application/models/Tour.php
@@ -12,32 +12,94 @@ class Tour extends \Framework\Shared\Model {
 	 */
 	protected $_id;
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type text
+	 * @length 255
+	 *
+	 * @validate required, min(3)
+	 * @label title
+	 */
 	protected $_title;
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type text
+	 * @length 255
+	 *
+	 * @label real title
+	 */
 	protected $_realTitle;
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type text
+	 *
+	 * @label description
+	 */
 	protected $_description;
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type text
+	 *
+	 * @label description local
+	 */
 	protected $_descriptionLocal;
 	
+	
 	/**
-	 * tour / festival
-	 * @var unknown_type
+	 * @column
+	 * @readwrite
+	 * @type integer
+	 *
+	 * @validate required
+	 * @related TourType
 	 */
 	protected $_type;
 	
 	/**
-	 * world / africa / america / East-Asia & Oceania / Europe / Middle East
-	 * @var unknown_type
+	 * @column
+	 * @readwrite
+	 * @type integer
+	 *
+	 * @validate required
+	 * @related Continent
 	 */
 	protected $_continent;
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type datetime
+	 *
+	 * @validate required
+	 * @label start
+	 */
 	protected $_start; // datetime
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @type datetime
+	 *
+	 * @validate required
+	 * @label end
+	 */
 	protected $_end; // datetime
 	
-	protected $_relatedArtists; // id
+	//protected $_relatedArtists; // id
 	
+	/**
+	 * @column
+	 * @readwrite
+	 * @length 255
+	 * @type text
+	 */
 	protected $_artistsKeywords; // a voir
 
 }
